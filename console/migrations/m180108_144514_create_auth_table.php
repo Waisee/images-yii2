a2150035146a5b664b6335fd4cfb578b50ec0e20
Danil Baluev <waisee7@gmail.com>
2018-01-08 23:13:35 +0500

    auth feature

---
+++ console/migrations/m180108_144514_create_auth_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `auth`.
+ */
+class m180108_144514_create_auth_table extends Migration
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('auth', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull(),
+            'source' => $this->string()->notNull(),
+            'source_id' => $this->string()->notNull(),
+        ]);
+
+        $this->addForeignKey('fk-auth-user_id-user-id', 'auth', 'user_id', 'user', 'id', 'CASCADE', 'CASCADE');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('auth');
+    }
+
+}
